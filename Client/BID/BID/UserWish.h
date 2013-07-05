ec40fba04f4365731f16d32d30a02ad933763b5b
fengyong <fengyong@qfpay.com>
2013-07-05 19:10:46 +0800

    add model

---
+++ Client/BID/BID/UserWish.h
@@ -0,0 +1,13 @@
+//
+//  UserWish.h
+//  BID
+//
+//  Created by brightman on 13-7-5.
+//  Copyright (c) 2013年 brightman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserWish : NSObject
+
+@end
---
+++ Client/BID/BID/UserWish.m
@@ -0,0 +1,18 @@
+//
+//  UserWish.m
+//  BID
+//
+//  Created by brightman on 13-7-5.
+//  Copyright (c) 2013年 brightman. All rights reserved.
+//
+
+#import "UserWish.h"
+
+@implementation UserWish
+-CreateUser
+-User
+-AddWish
+-Wishlist
+-RandWishlist
+-UpdateWish
+@end
