db44a3c0d55fbca31a3967c4248d33aff2dcc85e
fengyong <fengyong@qfpay.com>
2013-07-06 00:27:14 +0800

    Add Model

--- Client/BID/BID/UserWish.h
+++ Client/BID/BID/UserWish.h
@@ -10,4 +10,16 @@
 
 @interface UserWish : NSObject
 
+@property(nonatomic,strong)NSNumber *_uid;
+@property(nonatomic,strong)NSMutableArray *_wishlist;
+@property(nonatomic,strong)NSMutableArray *_randomwishlist;
+
+- (void) CreateUser:(NSString *)uuid withlng:(NSNumber *)lng lat:(NSNumber *)lat;
+//- (void) User
+- (void) AddWish:(NSString *)title withCost:(NSNumber *)cost uptodate:(NSDate *)uptodate;
+- (NSDictionary *) Wishlist;
+- (NSDictionary *) RandWishlist;
+- (BOOL)PubWish:(NSInteger)wid withPub:(Boolean)pub;
+- (BOOL)SupportWish:(NSInteger)wid;
+- (BOOL)OwnWish:(NSInteger)wid;
 @end
